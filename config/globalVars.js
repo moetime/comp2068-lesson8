6680d0424688a1b610de95d8e9d1d83df9877aa2
moetime <morrice_pfeiffer@hotmail.com>
2016-11-03 20:22:18 -0400

    Initial Commit

---
+++ config/globalVars.js
@@ -0,0 +1,10 @@
+/**
+ * Created by RFreeman on 10/6/2016.
+ */
+
+// global variables for the application
+module.exports = {
+  db: 'mongodb://localhost/comp2068-thu',
+  secret: 'UseThis to create Salt 123'
+  //db: 'mongodb://uname:pass@ds048319.mlab.com:48319/comp2068-thu'
+};
---
+++ public/javascripts/scripts.js
@@ -0,0 +1,6 @@
+/**
+ * Created by RFreeman on 10/13/2016.
+ */
+$('.confirmation').on('click', function() {
+    return confirm('Are you sure you want to delete this?');
+});
