54b6c167aec07af4367bb793743beb29f43e8175
moetime <morrice_pfeiffer@hotmail.com>
2016-11-06 18:40:16 -0500

    db commented out

--- config/globalVars.js
+++ config/globalVars.js
@@ -7,5 +7,5 @@ module.exports = {
   //db: 'mongodb://localhost/comp2068-thu',
  // secret: 'UseThis to create Salt 123'
   //db: 'mongodb://uname:pass@ds048319.mlab.com:48319/comp2068-thu'
-  db: 'mongodb: //Morrice:darlene@1234.mytest.canadaeast.cloudapp.azure.com:27017/mytestdb'
+ // db: 'mongodb: //Morrice:darlene@1234.mytest.canadaeast.cloudapp.azure.com:27017/mytestdb'
 };
