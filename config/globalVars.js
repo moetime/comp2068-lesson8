75b6e5755c203b212f50a67cf193a748e804b368
moetime <morrice_pfeiffer@hotmail.com>
2016-11-07 08:08:13 -0500

    connection change

--- config/globalVars.js
+++ config/globalVars.js
@@ -5,8 +5,8 @@
 // global variables for the application
 module.exports = {
   //db: 'mongodb://localhost/comp2068-thu',
- //   db: 'mongodb://mytest.canadaeast.cloudapp.azure.com:27017/mytestdb',
- db: 'mongodb://52.229.120.110/mytest.canadaeast.cloudapp.azure.com:27017/mytestdb',
+   db: 'mongodb://mytest.canadaeast.cloudapp.azure.com:27017/mytestdb',
+ //db: 'mongodb://52.229.120.110/mytest.canadaeast.cloudapp.azure.com:27017/mytestdb',
 
   //secret: 'UseThis to create Salt 123'
   //db: 'mongodb://uname:pass@ds048319.mlab.com:48319/comp2068-thu'
