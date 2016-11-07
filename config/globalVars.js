b79214ba95495011b9dc24c1f7ec34c6e5e28072
moetime <morrice_pfeiffer@hotmail.com>
2016-11-06 20:48:13 -0500

    connection string remove user id and pwd.

--- config/globalVars.js
+++ config/globalVars.js
@@ -5,7 +5,7 @@
 // global variables for the application
 module.exports = {
   //db: 'mongodb://localhost/comp2068-thu',
-  db: 'mongodb://Morrice:Darlene@1234.mytest.canadaeast.cloudapp.azure.com:27017/mytestdb'
+  db: 'mongodb: //mytest.canadaeast.cloudapp.azure.com:27017/mytestdb',
   //secret: 'UseThis to create Salt 123'
   //db: 'mongodb://uname:pass@ds048319.mlab.com:48319/comp2068-thu'
   
