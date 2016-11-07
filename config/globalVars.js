2ebbc9778db301059670da919636ff4c24369e9a
moetime <morrice_pfeiffer@hotmail.com>
2016-11-07 13:49:10 -0500

    db changes

--- config/globalVars.js
+++ config/globalVars.js
@@ -5,7 +5,7 @@
 // global variables for the application
 module.exports = {
   //db: 'mongodb://localhost/comp2068-thu',
- db: 'mongodb://mytest.canadaeast.cloudapp.azure.com:27017/mytestdb',
+ db: 'mongodb://Morrice:mytest.canadaeast.cloudapp.azure.com:27017/mytestdb',
  //  db:'mongodb://Morrice:Darlene@1234@52.229.120.110/mytest.canadaeast.cloudapp.azure.com:27017/mytestdb'
  //db: 'mongodb://52.229.120.110/mytest.canadaeast.cloudapp.azure.com:27017/mytestdb',
 
